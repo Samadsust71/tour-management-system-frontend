e796065a07bea706b34d35aed5def2af71fc25ae
Samadsust71 <samadsust71@gmail.com>
2025-08-04 18:54:58 +0600

    Initial commit on development branch

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>tour management </h1>
+      <Button>Click Me</Button>
+    </>
+  );
+}
+
+export default App;
