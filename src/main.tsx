bfaf55636b8811f0807b4758f08a25fa08fe9aca
Samadsust71 <samadsust71@gmail.com>
2025-08-10 03:20:46 +0600

    part-2 completed

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Provider as ReduxProvider } from "react-redux";
 import { ThemeProvider } from './providers/ThemeProvider'
 import { RouterProvider } from 'react-router'
 import { router } from './routes'
+import { Toaster } from './components/ui/sonner'
+import { store } from './redux/store';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
+    <ReduxProvider store={store}>
+      <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
         <RouterProvider router={router} />
+        <Toaster richColors position='top-center' />
       </ThemeProvider>
+    </ReduxProvider>
   </StrictMode>,
 )
